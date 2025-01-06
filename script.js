4256f79216777bd532d508052ee42913a0f4f4fb
tliu0075 <tliu0075@student.monash.com>
2025-01-06 10:23:41 +0800

    Add images to image file. Add about me and place to visit section.

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ menuOpenButton.addEventListener("click", ()=>{
     // Toggle mobile menu visibility
     document.body.classList.toggle("show-mobile-menu");
 });
-
+ 
 // close menu when the close button is clicked
 menuCloseButton.addEventListener("click", ()=>menuOpenButton.click());
 
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-// https://www.youtube.com/watch?v=MYFgtnKMDp4 
-// 23:09
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  });
+  
+// https://www.youtube.com/watch?v=MYFgtnKMDp4
+// 41
