8e306cf582f66dbd04be49bf5f767bfbc343cb2e
tliu0075 <tliu0075@student.monash.com>
2025-01-07 17:29:12 +0800

    Add reference to nav bar, add footer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -8,6 +9,12 @@ menuOpenButton.addEventListener("click", ()=>{
  
 // close menu when the close button is clicked
 menuCloseButton.addEventListener("click", ()=>menuOpenButton.click());
+
+// close menu when the nav link is clicked
+navLinks.forEach((link)=>{
+    link.addEventListener("click", ()=>menuOpenButton.click());
+});
+
 
 // Initialize Swiper
 const swiper = new Swiper('.slider-wrapper', {
